49caa591e5e161ee51888b0a730e3ebf72c4162b
Oleg Chursin <olegchursin@gmail.com>
2022-04-11 10:07:24 -0400

    feat: add checkbox and switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'uui-angular';
-  username: string = '';
+  title = 'United UI Web Components with Angular';
+  textInputValue: string = '';
+  switchValue = false;
+  checkboxValue = false;
+  checkboxes = [
+    {
+      id: 'cb-1',
+      name: 'uui-cb-1',
+      value: '1',
+      label: 'Tab 1',
+      disabled: false,
+      error: false,
+      checked: false
+    },
+    {
+      id: 'cb-2',
+      name: 'uui-cb-2',
+      value: '2',
+      label: 'Tab 2',
+      disabled: false,
+      error: false,
+      checked: true
+    },
+    {
+      id: 'cb-2',
+      name: 'uui-cb-2',
+      value: '2',
+      label: 'Tab 3 (disabled)',
+      disabled: true,
+      error: false,
+      checked: false
+    },
+    {
+      id: 'cb-2',
+      name: 'uui-cb-2',
+      value: '2',
+      label: 'Tab 4 (error)',
+      disabled: false,
+      error: true,
+      checked: false
+    }
+  ];
 
   public handleButtonClick() {
-    console.log('username', this.username);
     alert('Button clicked');
   }
 }
